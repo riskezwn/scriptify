e21b5b56658085e18bbe559701b42095a69c4ad1
riskezwn <enriquegarciagasc@gmail.com>
2022-04-25 21:59:45 +0200

    Funcion para cambiar de canciones

--- src/main.js
+++ src/main.js
@@ -1,15 +1,20 @@
 const songsList = [
-  "https://scummbar.com/mi2/MI1-CD/01%20-%20Opening%20Themes%20-%20Introduction.mp3",
-  "https://scummbar.com/mi2/MI1-CD/02%20-%20Chapter%20Screen.mp3",
-  "https://scummbar.com/mi2/MI1-CD/03%20-%20The%20Scumm%20Bar.mp3"
+  new Audio("https://scummbar.com/mi2/MI1-CD/01%20-%20Opening%20Themes%20-%20Introduction.mp3"),
+  new Audio("https://scummbar.com/mi2/MI1-CD/02%20-%20Chapter%20Screen.mp3"),
+  new Audio("https://scummbar.com/mi2/MI1-CD/03%20-%20The%20Scumm%20Bar.mp3")
 ];
 
 const playPauseButton = document.querySelector(".play");
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
 const playPauseIcon = document.querySelector(".play-icon");
-const url = songsList[1];
-const song = new Audio(url);
+
+let currentSong = 1;
+let song = songsList[currentSong];
 
 const playSong = () => {
+  /* url = songsList[currentSong];
+  song = new Audio(url); */
   if (song.paused) {
     playPauseIcon.classList.remove("fa-play");
     playPauseIcon.classList.add("fa-pause");
@@ -22,8 +27,26 @@ const playSong = () => {
   }
 };
 
+const prevSong = () => {
+  if (currentSong > 0) {
+    song.pause();
+    currentSong--;
+    song = songsList[currentSong];
+    updateSongData();
+  }
+};
+
+const nextSong = () => {
+  if (currentSong < songsList.length - 1) {
+    song.pause();
+    currentSong++;
+    song = songsList[currentSong];
+    updateSongData();
+  }
+};
+
 const updateSongData = () => {
-  const cleanSong = cleanName(url);
+  const cleanSong = cleanName(song.src);
   const artist = "Monkey Island";
 
   document.querySelector(".song-name").textContent = cleanSong;
@@ -35,3 +58,5 @@ const cleanName = (url) => {
 };
 
 playPauseButton.addEventListener("click", () => playSong());
+prevButton.addEventListener("click", () => prevSong());
+nextButton.addEventListener("click", () => nextSong());
